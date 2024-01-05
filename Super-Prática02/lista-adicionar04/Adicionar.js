555705fc33ae88b973586219886975887dffaadc
Jonhy Santos <Jonhy1905@hotmail.com>
2024-01-04 22:28:44 -0300

    Praticando sozinho 80%

--- Super-Prática02/lista-adicionar04/Adicionar.js
+++ Super-Prática02/lista-adicionar04/Adicionar.js
@@ -7,7 +7,7 @@ let valores = []
 let button = document.querySelector('.button')
 button.addEventListener('click',adicionar)
 
-// Verificando se o número está entre 1 e 100
+// Verificando se o número está entre 1 e 
 function isNumero(n){
     if(Number(n) >= 1 && Number(n) <= 100){
         return true
@@ -16,7 +16,7 @@ function isNumero(n){
     }
 }
 
-// Verificando se o número já consta em lista
+// Verificar se já consta em linsta
 function inLista(n,l){
     if(l.indexOf(Number(n)) != -1){
         return true
@@ -25,13 +25,64 @@ function inLista(n,l){
     }
 }
 
+
 // Função da ação
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value,valores)){
-        window.alert('tudo ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else{
         window.alert('Número inválido ou ja consta em lista.')
     }
+    num.value = ''
+    num.focus()
 }
+
+
+
+
+
+// Finalizar //
+// Ação do botão
+let button2 = document.querySelector('.button2')
+button2.addEventListener('click',finalizar)
+
+
+// Função da ação 
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores para finalizar.')
+    }else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+            
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrado.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}`
+        res.innerHTML += `<p>A média de todos os valores é ${media.toFixed(2)} </p>`
+
+
+    }
+}
+
+
+
+
 
 
