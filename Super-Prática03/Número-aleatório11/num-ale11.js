70702145756cc44faf985b46647778d4c50b7706
Jonhy Santos <Jonhy1905@hotmail.com>
2024-01-25 21:04:24 -0300

    fiz praticas

---
+++ Super-Prática03/Número-aleatório11/num-ale11.js
@@ -0,0 +1,25 @@
+// Gera um número aleatório.
+const numeroAleatorio = Math.floor(Math.random() * 100)+1
+
+// Número máximo de tentativas.
+const maxTentativas = 10
+let tentativas = 0
+
+// Criamos o loop 
+
+for(tentativas = 1; tentativas <= maxTentativas; tentativas ++){
+    const palpite = Number(prompt(`Tentativas ${tentativas} / ${maxTentativas}. Adivinhe o número (entre 1 e 100).`))
+    if(isNaN(palpite) || palpite < 1 || palpite > 100){
+        window.alert(`Por favor, insira um núero entre 1 e 100`)
+    }else if(palpite === numeroAleatorio){
+        window.alert(`Parabéns! você acertou o número ${numeroAleatorio} em ${tentativas} tentativas.`)
+    }else if(palpite < numeroAleatorio){
+        window.alert('Tente um número maior')
+    }else{
+        window.alert('Tente um número menor.')
+    }
+}
+
+if(tentativas > maxTentativas){
+    window.alert(`Suas ${maxTentativas} Tentativas acabaram, o número era ${numeroAleatorio}`)
+}
