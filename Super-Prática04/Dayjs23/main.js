2046475e0c3cdbb2bf797f7ab53af6e8f9f9f8f5
Jonhy Santos <Jonhy1905@hotmail.com>
2024-03-05 21:55:36 -0300

    mais uma pratica do dia

--- Super-Prática04/Dayjs23/main.js
+++ Super-Prática04/Dayjs23/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { calcularDiasRestantes, calcularIdade } from './funções23.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -20,5 +21,12 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
+
+const dataNascimento = prompt('Digite sua data de nascimento (AAAA-MM-DD)')
+
+const idade = calcularIdade(dataNascimento)
+const diasRestantes = calcularDiasRestantes(dataNascimento)
+
+alert(`Vocẽ tem ${idade} anos e restam ${diasRestantes} dias para o seu aniversário.`)
 
 setupCounter(document.querySelector('#counter'))
