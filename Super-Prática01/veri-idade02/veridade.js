d4b5dbcf625cc43a422c195a4208ad586010206b
Jonhy Santos <Jonhy1905@hotmail.com>
2024-03-20 01:36:45 -0300

    pratica verificador de idade

--- Super-Prática01/veri-idade02/veridade.js
+++ Super-Prática01/veri-idade02/veridade.js
@@ -1,31 +1,28 @@
 let date = new Date()
-let anoAtual = date.getFullYear()
-const nascimento = document.querySelector('#txtn')
+let dataAtual = date.getFullYear()
+let anoNascimento = document.querySelector('#txtn')
 let sexo = document.getElementsByName('radsex')
-let out = document.querySelector('#txtout')
-let btn = document.querySelector('#btn')
 let res = document.querySelector('#res')
+let outro = document.querySelector('#outro')
+let btn = document.querySelector('#btn')
+btn.addEventListener('click', verificar)
 
-// Ação do botão.
-btn.addEventListener('click',verificar)
-
-// Função da ação.
 function verificar(){
-    if(nascimento.value.length == 0 ){
-        window.alert('Por favor, insira uma data válida.')
+    if(anoNascimento.value == 0){
+        window.alert('Por favor, digite o ano de nascimento ex: (2014)')
     }else{
-        let idade = anoAtual -  Number(nascimento.value)
-        let Gênero = ''
+        let idade =  Number(dataAtual) - Number(anoNascimento.value)
+        let Gênero = ""
         let foto = document.createElement('img')
         foto.setAttribute('id','foto')
-        if(sexo[0].checked){
+        if(sexo[0].checked ){
             Gênero = 'Homem'
             if(idade > 0 && idade < 12){
                 Gênero = 'Menino'
                 foto.setAttribute('src','./imagens/criança-m.png')
             }else if(idade < 21){
                 foto.setAttribute('src','./imagens/jovem-m.png')
-            }else if(idade < 50 ){
+            }else if(idade < 50){
                 foto.setAttribute('src','./imagens/adulto-m.png')
             }else{
                 foto.setAttribute('src','./imagens/idoso-m.png')
@@ -42,11 +39,8 @@ function verificar(){
             }else{
                 foto.setAttribute('src','./imagens/idosa-f.png')
             }
-        }else{
-            let out = out.value
         }
-
-        res.innerHTML = `<p>Você nasceu em ${nascimento.value} e tem ${idade} anos e do Genero ${Gênero}</p>`
+        res.innerHTML = `<p>Detectamos ${Gênero} que nasceu em ${anoNascimento.value} e em ${dataAtual} completará ou já completou ${idade}  anos </p>`
         res.appendChild(foto)
     }
 }
