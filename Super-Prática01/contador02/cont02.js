9e2c0476fa3572acb89f4a962e821f25979bb968
Jonhy Santos <Jonhy1905@hotmail.com>
2024-03-21 16:28:23 -0300

    pratica.

---
+++ Super-Prática01/contador02/cont02.js
@@ -0,0 +1,30 @@
+let inicio = document.querySelector('#txti')
+let fim = document.querySelector('#txtf')
+let passo = document.querySelector('#txtp')
+let res = document.querySelector('#res')
+let btn = document.querySelector('#btn')
+
+btn.addEventListener('click',contar)
+
+function contar(){
+    if(inicio.value == 0 || fim.value == 0 || passo.value == 0){
+        window.alert('Faltam dados, por favor preencha todos os dados.')
+    }else{
+        res.innerHTML = ''
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(i < f){
+            // ordem crescente
+            for(let c = i;c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }else{
+            // ordem decrescente
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
+}
