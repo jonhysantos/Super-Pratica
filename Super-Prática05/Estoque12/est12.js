1732ddb05e27932c252a45530095dd1f9ef996a7
Jonhy Santos <Jonhy1905@hotmail.com>
2024-04-21 11:57:55 -0300

    pratica no domingo.

--- Super-Prática05/Estoque12/est12.js
+++ Super-Prática05/Estoque12/est12.js
@@ -1,16 +1,4 @@
-const estoque = []
-
 function cadastrarItem(){
-    const nome = prompt('Digite o nome do item:')
-    const quantidade = parseInt('Digite a quantidade do item:')
-
-    if(isNaN(quantidade) || quantidade <= 0 ){
-        alert('Quantidade inválida.')
-        return
-    }
-
-    // Mesmo que {nome:nome,quantidade:qantidade}
-    const item = {nome,quantidade}
-    estoque.push(item)
-    alert(`${nome} foi adicionado com ${quantidade} unidades.`)
+    nome = prompt('Digite o nome do item:')
+    quantidade = parseI(prompt('Digite a quantidade do item:'))
 }
