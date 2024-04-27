043e3d24f63062d8fa1bcbc2ae0343d057fc938a
Jonhy Santos <Jonhy1905@hotmail.com>
2024-04-27 16:43:10 -0300

    pratica do dia

---
+++ Super-Prática05/Estoque16/est16.js
@@ -0,0 +1,11 @@
+const estoque =[]
+
+function cadastrarItem(){
+    const nome = prompt('Digite o nome do item:')
+    const quantidade = parseInt(prompt('Digite a quantidade do item:'))
+
+    // Mesmo que {nome:nome,quantidade:quantidade}
+    const item = {nome,quantidade}
+    estoque.push(item)
+    alert(`${nome} foi cadastrado no estoque com ${quantidade} unidades.`)
+}
