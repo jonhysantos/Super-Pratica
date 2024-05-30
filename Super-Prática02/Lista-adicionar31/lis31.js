b29d15f31c3cf2cc9c032b2834abbd10e87ba684
jonhy santos <jonhy1905@hotmail.com>
2024-05-30 11:11:42 -0300

    pratica do dia

--- Super-Prática02/Lista-adicionar31/lis31.js
+++ Super-Prática02/Lista-adicionar31/lis31.js
@@ -25,8 +25,41 @@ function inLista(n,l){
 
 function adicionar(){
     if(isNum(num.value) && !inLista(num.value,valores)){
-        alert('tudo ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+        num.value = ''
+        num.focus()
     }else{
         alert('Por favor apenas números.')
+    }
+}
+
+btn2.addEventListener('click',finalizar)
+
+function finalizar(){
+    if(valores.length === 0){
+        alert('Adicione valores antes de finalizar.')
+    }else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / total
+        res.innerHTML = `<p>No total, temos ${total} números cadastrados</p>`
+        res.innerHTML += `<p>O mairo valor informado ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A média é ${media.toFixed(2)}.</p>`
     }
 }
