56204f0990e660e0a442e889516cb1f4c152c66d
Jonhy <jonhy1905@hotmail.com>
2024-06-17 15:03:23 -0300

    mantendo a constacia.

--- Super-Prática05/Estoque31/est31.js
+++ Super-Prática05/Estoque31/est31.js
@@ -1,5 +1,6 @@
 const estoque = [];
 
 function cadastrarItem(){
-    const nome
+    const nome = prompt('Digite o nome do item que deseja cadastrar:')
+    const quantidade = parseInt(prompt('Digite a quantidade do produto:'))
 }
