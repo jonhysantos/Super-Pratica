57e6cc725bc335f2510a5980975872f90928b336
Jonhy Santos <jonhy1905@hotmail.com>
2024-06-21 20:59:19 -0300

    mantendo a constancia do dia.

---
+++ Super-Prática06/Lista-filmes16/lis16.js
@@ -0,0 +1,16 @@
+const inputFilme = document.getElementById('inputFilme');
+const btn = document.getElementById('btn');
+const ulElement = document.getElementById('ulElement');
+
+btn.addEventListener('click',function(){
+    if(inputFilme.value.length === 0){
+        alert(`Por favor, insira o nome antes de adicionar.`)
+        return
+    }
+
+    const liElement = document.createElement('li');
+    const nomeFilme = inputFilme.value
+    liElement.textContent = nomeFilme
+    ulElement.appendChild(liElement);
+
+})
