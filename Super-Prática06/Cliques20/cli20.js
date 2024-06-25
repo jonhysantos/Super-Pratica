fca03972de3ac0624f3e4a5818f02fbeed834371
Jonhy Santos <jonhy1905@hotmail.com>
2024-06-24 21:03:08 -0300

    Praticanco aos poucos.

--- Super-Prática06/Cliques20/cli20.js
+++ Super-Prática06/Cliques20/cli20.js
@@ -1,11 +1,10 @@
 const paragrafo = document.getElementById('paragrafo');
 const btn = document.getElementById('btn');
-const ulElement = document.getElementById('ulElement')
 
-let contador = 0
+
+let contador = 0;
 
 btn.addEventListener('click',function(){
-    contador ++
-    const liElement = document.createElement('li');
-    paragrafo.textContent = `${contador} cliques${contador !== 1 ? "s": ""}`
+    contador ++;
+    paragrafo.textContent = `${contador} clique${contador !== 1 ? "s" : ""}`
 })
