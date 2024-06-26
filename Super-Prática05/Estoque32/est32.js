0b3ed3b3628e26a8fe5ae6b657be10b1ececec3b
Jonhy Santos <jonhy1905@hotmail.com>
2024-06-25 22:06:12 -0300

    pratica de estoque e lista de filmes.

---
+++ Super-Prática05/Estoque32/est32.js
@@ -0,0 +1,84 @@
+const estoque = []
+
+function cadastrarItem(){
+    const nome = prompt('Digite o nome do item que deseja cadastrar no estoque:');
+    const quantidade = prompt('Digite a quantidade do item:');
+
+    // Mesmo que {nome:nome,quantidade:quantidade}
+    const item = {nome,quantidade}
+    estoque.push(item);
+    alert(`${nome} foi adicionado ao estoque com ${quantidade} unidades.`)
+}
+
+function listarItens(){
+    if(estoque.length === 0){
+        alert(`O estoque está vazio`)
+        return
+    }
+    
+    // Transforma uma lista de objetos em uma lista de strings e depous junta tudo
+    const itensStrings = estoque.map(function(item,index){
+        return `${index + 1}. ${item.nome}: ${item.quantidade} unidades`
+    }).join('\n')
+    alert(`itens no estoque.\n ${itensStrings}`);
+}
+
+function alterarQuantidade(){
+    listarItens()
+    const indice = parseInt(prompt('Digite o número do item que deseja alterar a quantidade')) - 1
+
+    if(isNaN(indice) || indice < 0 || indice >= estoque.length){
+        alert('Número do item inválido')
+        return
+    }
+
+    const novaQuantidade = parseInt(prompt(`Digite a nova quantidade para ${estoque[indice].nome}`));
+
+    if(isNaN(novaQuantidade) || novaQuantidade < 0){
+        alert(`Quantidade inválida,tem não alterado`)
+        return
+    }
+
+    estoque[indice].quantidade = novaQuantidade
+    alert(`"${estoque[indice].nome}" agora posuui ${novaQuantidade}`)
+}
+
+function excluirItem(){
+    listarItens()
+    const indice = parseInt(prompt('Digite o número do item que deseja excluir do estoque:')) - 1
+
+    if(isNaN(indice) || indice < 0 || indice >= estoque.length){
+        alert('Número do item inválido.')
+        return
+    }
+
+    const itemExcluido = estoque[indice].nome
+    estoque.splice(indice,1)
+    alert(`"${itemExcluido}" foi excluído do estoque`)
+}
+
+let escolha 
+
+while(escolha !== "5"){
+    escolha = prompt(`Escolha uma opção:\n1.Cadastrar novo item.\n2.listar itens do estoque.\n3.Alterar a quantidade do item.\n4.Excluir item do estoque.\n5.Sair`)
+
+    switch(escolha){
+        case "1":
+            cadastrarItem()
+            break
+        case "2":
+            listarItens()
+            break
+        case "3":
+            alterarQuantidade()
+            break
+        case "4":
+            excluirItem()
+            break
+        case "5":
+            alert('Saindo do programa de gerenciamento de estoque.')
+            break
+        default:
+            alert('Opção inválida,tente novamente.')
+    }
+}
---
+++ Super-Prática06/Lista-Filmes17/lis17.js
@@ -0,0 +1,14 @@
+const inputFilme = document.getElementById('inputFilme');
+const btn = document.getElementById('btn');
+const ulElement = document.getElementById('ulElement');
+
+btn.addEventListener('click',function(){
+    if(inputFilme.value.length === 0){
+        alert(`Por favor, insira o nome do filme antes de adicionar`)
+        return
+    }
+    const liElement = document.createElement('li');
+    const nomeFilme = inputFilme.value
+    liElement.textContent = nomeFilme
+    ulElement.appendChild(liElement)
+})
