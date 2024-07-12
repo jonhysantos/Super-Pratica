89005bae8dbd56e2ab39ae78704e445cf19a6c51
Jonhy Santos <jonhy1905@hotmail.com>
2024-07-12 19:23:49 -0300

    pratica do dia

--- Super-Prática07/Lista-tarefas00/index.js
+++ Super-Prática07/Lista-tarefas00/index.js
@@ -1,49 +1,30 @@
-const formTarefa = document.getElementById('formTarefa');
-formTarefa.addEventListener('submit',adicionarTarefas);
-
-// Reaproveitando a função usada no exercício anterior
 function toKebabCase(str){
-    return str.split(" ").join("-").toLowerCase()
+    return str.split(" ").join('-').toLowerCase()
 }
 
-// Função para adicionar uma nova tarefa a lista.
-function adicionarTarefas(event){
-    event.preventDefault();
+function adicionarLi(jogo){
+    const liElement = document.createElement('li');
+    liElement.id = toKebabCase(jogo.nome)
+    liElement.innerHTML = `Nome: ${jogo.nome} <br> Ano de lançamento: ${jogo.ano} <br> Gênero: ${jogo.genero}`
 
-    const inputTarefa = document.getElementById('inputTarefa');
-    const textoTarefa = inputTarefa.value;
-    if(textoTarefa !== ""){
-
-        const listaTarefas = document.getElementById('listaTarefas');
-        const novaTarefa = document.createElement('li');
-
-        // Cria um input de checkbox para marcar como concluído.
-        const checkbox = document.createElement('input');
-        checkbox.id = toKebabCase(textoTarefa)
-        checkbox.type = "checkbox";
-        checkbox.addEventListener("change",function () {
-            novaTarefa.classList.toggle("concluido");
-        });
-
-        // Cria uma label com a tarefa.
-        const label = document.createElement('label');
-        label.htmlFor = toKebabCase(textoTarefa);
-        label.textContent = textoTarefa
-
-        novaTarefa.append(checkbox,label);
-
-        // Adiciona um botão para excluir a tarefa.
-        const botaoExcluir = document.createElement('button');
-        botaoExcluir.textContent = "Excluir";
-        botaoExcluir.addEventListener('click',function () {
-            novaTarefa.remove();
-
-        });
-
-        novaTarefa.appendChild(botaoExcluir);
-        listaTarefas.appendChild(novaTarefa);
-        event.target.reset()
-    }
-
-
+    document.getElementById('ulElement').appendChild(liElement)
 }
+
+const form = document.getElementById('cadastroJogos')
+
+form.addEventListener('submit',function(evento){
+    evento.preventDefault()
+    const inputNome = document.getElementById('inputNome');
+    const inputAno = document.getElementById('inputAno');
+    const inputGenero = document.getElementById('inputGenero');
+
+    const jogo = {}
+
+    jogo.nome = inputNome.value
+    jogo.ano = Number(inputAno.value)
+    jogo.genero = inputGenero.value
+
+    adicionarLi(jogo)
+
+    evento.target.reset()
+})
