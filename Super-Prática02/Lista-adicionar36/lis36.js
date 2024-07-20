32e8b8b246351fd3922ed8fe0f6d798eefb8bd69
Jonhy Santos <jonhy1905@hotmail.com>
2024-07-20 17:14:21 -0300

    Sabado de estudo.

---
+++ Super-Prática02/Lista-adicionar36/lis36.js
@@ -0,0 +1,65 @@
+const txtn = document.getElementById('txtn');
+const btn = document.getElementById('btn');
+const lista = document.getElementById('lista');
+const btn2 = document.getElementById('btn2');
+const res = document.getElementById('res');
+const valores = [];
+
+btn.addEventListener('click',adicionar)
+
+function isNum(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n,l){
+    if(l.indexOf(Number(n)) !== -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNum(txtn.value) && !inLista(txtn.value,valores)){
+        valores.push(Number(txtn.value))
+        const option = document.createElement('option');
+        option.innerHTML = `${txtn.value} foi adiconado`
+        lista.appendChild(option)
+        txtn.value = ``
+        txtn.focus()
+    }else{
+        alert(`Número inválido ou já consta em lista`)
+    }
+}
+
+btn2.addEventListener('click',finalizar)
+
+function finalizar(){
+    if(valores.length == 0){
+        alert(`Por favor, adicione valores antes de finalizar`)
+        return 
+    }
+    let total = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
+    for(let pos in valores){
+        soma += valores[pos]
+        if(valores[pos] > maior)
+            maior = valores[pos]
+        if(valores[pos] < menor)
+            menor = valores[pos]
+        
+    }
+    media = soma / total
+    res.innerHTML = `<p>Ao todo,temos ${total} números cadastrados</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+    res.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+    res.innerHTML += `<p>A média é ${media}</p>`
+}
