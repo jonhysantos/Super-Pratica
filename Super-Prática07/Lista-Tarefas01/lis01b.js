1f20f3bd3a6a0b64f8bf3e36b927405c15c72115
Jonhy Santos <jonhy1905@hotmail.com>
2024-07-24 15:20:48 -0300

    Praticando .

--- Super-Prática07/Lista-Tarefas01/lis01b.js
+++ Super-Prática07/Lista-Tarefas01/lis01b.js
@@ -1,47 +1,45 @@
 const formTarefa = document.getElementById('formTarefa');
-formTarefa.addEventListener('submit',adicionarTarefa)
+formTarefa.addEventListener('submit',adicionarTarefa);
 
-// Reaproveitando a função aprendida da aula
 function toKebabCase(str){
     return str.split(' ').join('-').toLowerCase()
 }
 
-// Função para adicionar uma nova tarefa à lista.
 function adicionarTarefa(event){
-    event.preventDefault();
+    event.preventDefault()
 
     const inputTarefa = document.getElementById('inputTarefa');
-    const textoTarefa = inputTarefa.value
+    const textoTarefa = inputTarefa.value;
 
-    if(textoTarefa !== ''){
+    if(textoTarefa !== ""){
         const listaTarefas = document.getElementById('ulElement');
         const novaTarefa = document.createElement('li');
 
-        //Cria um input do tipo checkbox para marcar como concluído.
+        // Cria um input do tipo Checkbox para marcar como concluido.
+
         const checkbox = document.createElement('input');
         checkbox.id = toKebabCase(textoTarefa);
-        checkbox.type = 'checkbox';
-        checkbox.addEventListener('change',function(){
+        checkbox.type = "checkbox";
+        checkbox.addEventListener('click',function(){
             novaTarefa.classList.toggle('concluido');
         });
 
-        // Cria um label com a tarefa.
+        // Cria uma label com a tarefa
         const label = document.createElement('label');
         label.htmlFor = toKebabCase(textoTarefa);
         label.textContent = textoTarefa
 
         novaTarefa.append(label,checkbox);
 
-        // Adiciona um botão de excluir a tarefa.
+        // Adiciona um botão de excluir a tarefa
         const botaoExcluir = document.createElement('button');
-        botaoExcluir.textContent = `Excluir`;
+        botaoExcluir.textContent = 'Excluir';
         botaoExcluir.addEventListener('click',function(){
             novaTarefa.remove();
         });
+
         novaTarefa.appendChild(botaoExcluir);
         listaTarefas.appendChild(novaTarefa);
-
         inputTarefa.value = ''
-
     }
 }
--- Super-Prática07/Lista-jogos15/lis16.js
+++ Super-Prática07/Lista-jogos15/lis16.js
@@ -2,29 +2,29 @@ function toKebabCase(str){
     return str.split(' ').join('-').toLowerCase()
 }
 
-function adicionarli(jogo){
+function adicionarLi(jogo){
     const liElement = document.createElement('li');
-    liElement.id = toKebabCase(jogo.nome);
-    liElement.innerHTML = `Nome: ${jogo.nome} <br> Ano de lançamento: ${jogo.ano} <br> Gênero: ${jogo.genero}`
+    liElement.id = toKebabCase(jogo.nome)
+    liElement.innerHTML = `Nome: ${jogo.nome} <br> Ano de lançamento: ${jogo.ano} <br> Genero: ${jogo.genero}`
     document.getElementById('ulElement').appendChild(liElement)
 }
 
 const form = document.getElementById('cadastroJogos');
-
 form.addEventListener('submit',function(evento){
-    evento.preventDefault()
+    evento.preventDefault();
+
     const inputNome = document.getElementById('inputNome');
     const inputAno = document.getElementById('inputAno');
     const inputGenero = document.getElementById('inputGenero');
 
     const jogo = {}
 
-    jogo.nome = inputNome.value
-    jogo.ano = inputAno.value
-    jogo.genero = inputGenero.value
+    jogo.nome = inputNome.value;
+    jogo.ano = Number(inputAno.value);
+    jogo.genero = inputAno.value;
 
-    adicionarli(jogo)
+    adicionarLi(jogo)
 
-    form.target.reset()
+    evento.target.reset()
 })
 
