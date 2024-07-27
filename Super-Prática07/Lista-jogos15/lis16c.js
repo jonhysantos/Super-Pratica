7a5e33dbd7f1a77b3acba0c05cd4f09e3d1c319a
Jonhy Santos <jonhy1905@hotmail.com>
2024-07-26 21:54:36 -0300

    pratica do dia.

---
+++ Super-Prática07/Lista-jogos15/lis16c.js
@@ -0,0 +1,30 @@
+function toKebabCase(str){
+    return str.split(' ').join('-').toLowerCase()
+}
+
+function adicionarLi(jogo){
+    const liElement = document.createElement('li');
+    liElement.id = toKebabCase(jogo.nome);
+    liElement.innerHTML = `Nome: ${jogo.nome} <br> Ano de lançamento: ${jogo.ano} <br> Genero: ${jogo.genero}`
+    document.getElementById('ulElement').appendChild(liElement);
+
+}
+
+const form = document.getElementById('cadastroJogos');
+form.addEventListener('submit',function(evento){
+    evento.preventDefault();
+
+    const inputNome = document.getElementById('inputNome');
+    const inputAno = document.getElementById('inputAno');
+    const inputGenero = document.getElementById('inputGenero');
+
+    const jogo = {}
+
+    jogo.nome = inputNome.value;
+    jogo.ano = Number(inputAno.value);
+    jogo.genero = inputGenero.value;
+
+    adicionarLi(jogo)
+
+    evento.target.reset();
+})
