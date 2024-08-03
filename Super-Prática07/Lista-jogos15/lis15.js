c9ffede754c31ad154b2f67829641cc812500e09
Jonhy Santos <jonhy1905@hotmail.com>
2024-08-03 13:12:28 -0300

    pratica.

--- Super-Prática07/Lista-jogos15/lis15.js
+++ Super-Prática07/Lista-jogos15/lis15.js
@@ -2,28 +2,28 @@ function toKebabCase(str){
     return str.split(' ').join('-').toLowerCase()
 }
 
-function adicionarLi(jogo){
+function adicionarli(jogo){
     const liElement = document.createElement('li');
     liElement.id = toKebabCase(jogo.nome);
-    liElement.textContentL = `Nome: ${jogo.nome} <br> Ano de lançamento: ${jogo.ano} <br> Gênero: ${jogo.genero}`
+    liElement.innerHTML = `Nome: ${jogo.nome} <br> Ano de lançamento: ${jogo.ano} <br> Gênero ${jogo.genero}`
+
     document.getElementById('ulElement').appendChild(liElement)
 }
 
-const form = document.getElementById('cadastroJogos')
-
-form.addEventListener('submit',function(evento){
-    evento.preventDefault()
+const form = document.getElementById('cadastroJogos');
+form.addEventListener('submit',function(event){
+    event.preventDefault();
     const inputNome = document.getElementById('inputNome');
     const inputAno = document.getElementById('inputAno');
     const inputGenero = document.getElementById('inputGenero');
 
     const jogo = {}
 
-    jogo.nome = inputNome.value
-    jogo.ano = inputAno.value
-    jogo.genero = inputGenero.value
+    jogo.nome = inputNome.value;
+    jogo.ano = inputAno.value;
+    jogo.genero = inputGenero.value;
 
-    adicionarLi(jogo)
-    
-    evento.target.reset()
+    adicionarli(jogo);
+
+    event.target.reset();
 })
