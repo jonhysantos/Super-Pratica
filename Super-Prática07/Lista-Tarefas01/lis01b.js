f3cbdb7395da60a349a57ab4152cf1178e029b53
Jonhy Santos <jonhy1905@hotmail.com>
2024-08-05 13:10:31 -0300

    pratica de segunda.

--- Super-Prática07/Lista-Tarefas01/lis01b.js
+++ Super-Prática07/Lista-Tarefas01/lis01b.js
@@ -1,5 +1,5 @@
-const formTarefa = document.getElementById('formTarefa');
-formTarefa.addEventListener('submit',adicionarTarefa);
+const formTarera = document.getElementById('formTarefa');
+formTarera.addEventListener('submit',adicionarTarefa);
 
 // Reaproveitando a função da aula.
 function toKebabCase(str){
@@ -8,26 +8,41 @@ function toKebabCase(str){
 
 // Função para adicionar uma nova tarefa a lista.
 function adicionarTarefa(event){
-    event.preventDefaut();
+    event.preventDefault();
 
     const inputTarefa = document.getElementById('inputTarefa');
     const textoTarefa = inputTarefa.value;
 
     if(textoTarefa !== ""){
-        const listaTarefas = 
-        // Cria um input do tipo checkbox para marcar como concluido
-        const checkbox = document.getElementById('input');
+        const listaTarefa = document.getElementById('ulElement')
+        const novaTarefa = document.createElement('li')
+
+        // Cria uma checkbox para marcar como concluido.
+        const checkbox = document.createElement('input');
         checkbox.id = toKebabCase(textoTarefa);
-        checkbox.type = 'checkbox'
+        checkbox.type = 'checkbox';
         checkbox.addEventListener('change',function(){
-            checkbox.classList.toggle('concluido');
+            novaTarefa.classList.toggle('concluido')
         });
 
         // Cria uma label com a tarefa.
         const label = document.createElement('label');
+        label.id = toKebabCase(textoTarefa);
         label.htmlFor = toKebabCase(textoTarefa)
         label.textContent = textoTarefa
 
-        nov
+        novaTarefa.append(checkbox,label);
+
+        // Adiciona um botão de excluir a nova tarefa.
+        const excluir = document.createElement('button');
+        excluir.textContent = 'Excluir'
+        excluir.addEventListener('click',function(){
+            novaTarefa.remove();
+        });
+
+        novaTarefa.appendChild(excluir);
+        listaTarefa.appendChild(novaTarefa);
+        inputTarefa.value = ''
+        
     }
 }
