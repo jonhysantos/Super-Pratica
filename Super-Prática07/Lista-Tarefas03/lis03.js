bbdf367c2f8cb365ee314d23f7319bfb2abb9604
Jonhy Santos <jonhy1905@hotmail.com>
2024-08-27 01:25:20 -0300

    pratica do dia.

---
+++ Super-Prática07/Lista-Tarefas03/lis03.js
@@ -0,0 +1,44 @@
+const formTarefa = document.getElementById('formTarefa');
+formTarefa.addEventListener('submit',adicionarTarefa);
+
+function toKebabCase(str){
+    return str.split(' ').join('-').toLowerCase()
+}
+
+function adicionarTarefa(event){
+    event.preventDefault();
+
+    const inputTarefa = document.getElementById('inputTarefa');
+    const textoTarefa = inputTarefa.value;
+
+    if(textoTarefa !== ""){
+        const novaTarefa = document.createElement('li');
+        const listaTarefa = document.getElementById('ulElement');
+
+        // Cria um input do tipo checkbox para marcar como concluído.
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = toKebabCase(textoTarefa);
+        checkbox.addEventListener('change',function(){
+            novaTarefa.classList.toggle('concluido');
+        });
+
+        // Cria uma label com a nova tarefa.
+        const label = document.createElement('label');
+        label.htmlFor = toKebabCase(textoTarefa);
+        label.textContent = textoTarefa;
+
+        novaTarefa.append(checkbox,label);
+
+        // Adiciona um botão para remover a nova tarefa.
+        const botaoExcluir = document.createElement('button');
+        botaoExcluir.textContent = 'Excluir';
+        botaoExcluir.addEventListener('click',function(){
+            novaTarefa.remove();
+        });
+
+        listaTarefa.appendChild(novaTarefa);
+        novaTarefa.appendChild(botaoExcluir);
+        inputTarefa.value = ''
+    }
+}
