8ba73309e981286aae766dcdd6b0a452ccf61075
Jonhy Santos <jonhy1905@hotmail.com>
2024-09-07 16:19:23 -0300

    pratica de sabado.

--- Super-Prática07/Lista-tarefas00/ind.js
+++
@@ -1,9 +0,0 @@
-const formTarera = document.getElementById('formTarefa');
-formTarera.addEventListener('submit',adicionarTarefa);
-
-function adicionarTarefa(event){
-    event.preventDefault();
-    const inputTarefa = document.getElementById('inputTarefa');
-    const listaTarefa = document.getElementById('ulElement');
-    if()
-}
