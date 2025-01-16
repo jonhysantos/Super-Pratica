e75bbd22e05621a3e8b12a9ae4557be953dc1e6c
Jonhy Santos <jonhy1905@hotmail.com>
2025-01-16 11:22:06 -0300

    pratica do react.

---
+++ React/aula01-React/react/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+    const message = "primeiro projeto em react"
+
+    return (
+      <div>
+        <input placeholder="O que devo fazer..." />
+        <button>Adicionar</button>
+        <ul>
+          <li>Fazer jejum de dopamina</li>
+          <li>Estudar react</li>
+        </ul>
+
+      </div>
+    )
+
+
+}
+
+
+  
+
+export default App
