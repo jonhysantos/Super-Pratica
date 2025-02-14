044ec7584dc863cfb73abd41c717ca875ff47a90
Jonhy Santos <jonhy1905@hotmail.com>
2025-02-14 02:11:08 -0300

    pratica de javaScript puro.

--- Super-Prática05/Estoque40/index.js
+++ Super-Prática05/Estoque40/index.js
@@ -1,48 +1,33 @@
 const stock = []
 
-function registerProducts(){
+function registerProduct(){
     const name = prompt('Digite o nome do produto:')
     const quantity = parseInt(prompt(`Digite a quantidade para ${name}:`))
 
     if(isNaN(quantity) || quantity < 0){
-        alert('Quantidade inválida')
+        alert('Quantidade invalida')
         return
     }
 
+    // mesmo que {name:name,quantity:quantity}
     const item = {name,quantity}
     stock.push(item)
-    alert(`${name} foi cadastrado no estoque com ${quantity} unidades`)
+    alert(`${name} foi adicionado ao estoque com ${quantity} unidades`)
 }
 
-function itemList(){
-    if(stock.length === 0){
-        alert('O estoque está vazio')
-        return
-    }
-
-    // Transforma uma lista de strings em uma lista de objetos e depois junta tudo.
-    const itensStrings = stock.map(function(item,index) {
-        return `${index + 1} ${item.name} ${item.quantity} unidades`
-    }).join('\n')
-    alert(`Itens no estoque\n ${itensStrings}`)
-}
-
-let choice
+let choice 
 
 while(choice !== "5"){
-    choice = prompt('Escolha uma opção:\n1.Cadastrar um item.\n2.Listar itens no estoque.\n3.Alterar quantidade.\n4.Exluir item do estoque.\n5.Sair')
+    choice = prompt(`Escolha uma opção:\n1.Cadastrar item.\n2.Listar itens.\n3.Alterar quantiade.\n4.Excluir um item.\n5.sair`)
 
     switch(choice){
         case "1":
-            registerProducts()
-            break
-        case "2":
-            itemList()
+            registerProduct()
             break
         case "5":
-            alert('Saindo do program de gerenciamento de estoque.')
+            alert('Saindo do programa de gerenciamento de estoque.')
             break
         default:
-            alert('Opção inválida,tente novamente')
+            alert('Opção inválida')
     }
 }
--- Super-Prática05/Estoque40/index01.js
+++ Super-Prática05/Estoque40/index01.js
@@ -1,48 +1,87 @@
 const stock = []
 
-function registerProducts(){
+function registerProduct(){
     const name = prompt('Digite o nome do produto:')
-    const quantity= parseInt(prompt(`Digite a quantidade para ${name}:`))
+    const quantity = parseInt(prompt(`Digite a quantidade para ${name}:`))
 
-    if(isNaN(quantity) || quantity < 0 || quantity.length == 0 ){
-        alert('Quantidade inválida')
+    if(isNaN(quantity) || quantity < 0){
+        alert('Quantidade inválida,item não cadastrado.')
+        return
     }
 
-    // Mesmo que {name:name,quantity:quantity}
+    // mesmo que {name:name,quantity:quantity}
     const item = {name,quantity}
     stock.push(item)
-    alert(`${name} foi cadastrado no estoque com ${quantity} unidades.`)
+    alert(`${name} foi cadastrado no estoque com ${quantity} unidades`)
 }
 
 function itemList(){
-    if(stock.length === 0){
+    if(stock.length == 0){
         alert('O estoque está vazio')
         return
     }
 
-    // Transforma uma lista de strings em uma lista de objetos e depois junta tudo.
+    // Transforma uma lista de objetos em uma lista de strings e depois junta tudo
     const itensStrings = stock.map(function(item,index){
-        return `${index + 1} ${item.name}: ${item.quantity} unidades`
+        return `${index + 1} ${item.name}: ${item.quantity}`
     }).join('\n')
-    alert(`itens no estoque. \n ${itensStrings}`)
+    alert(`Item no estoque.\n ${itensStrings}`)
+}
+
+function changeQuantity(){
+    itemList()
+    const indice = parseInt(prompt('Digite o número do item que deseja alterar quantidade.')) - 1
+    if(isNaN(indice) || indice < 0 || indice >= stock.length){
+        alert('Item inexistente')
+        return
+
+    }
+
+    const newQuantity = parseInt(prompt(`Digite a nova quantidade para ${stock[indice].name}:`))
+    if(isNaN(newQuantity) || newQuantity < 0 ){
+        alert('Qunatidade inválida')
+        return
+    }
+
+    stock[indice].quantity = newQuantity
+    alert(`${stock[indice].name} agora possui ${newQuantity}`)
+}
+
+function deleteProduct(){
+    itemList()
+    const indice = parseInt(prompt('Digite o número do item que deseja alterar a quantidade')) - 1
+    if(isNaN(indice) || indice < 0 || indice >= stock.length){
+        alert('Número do item inexistente')
+        return
+    }
+
+    const itemDelete = stock[indice].name
+    stock.splice(indice,1)
+    alert(`${itemDelete} foi excluido do estoque.`)
 }
 
 let choice 
 
 while(choice !== "5"){
-    choice = prompt(`Escolha uma opção:\n1.Cadastrar item no estoque.\n2.Listar itens no estoque.\n3.Alterar quantidade.\n4.Excluir item do estoque.\n5.Sair`)
+    choice = prompt("Escolha uma opção:\n1.Cadastrar item no estoque.\n2.listar itens no  estoque.\n3.Alterar a quantidade de algum item.\n4.Excluir um item.\n5.Sair")
 
     switch(choice){
         case "1":
-            registerProducts()
+            registerProduct()
             break
         case "2":
             itemList()
+            break
+        case "3":
+            changeQuantity()
+            break
+        case "4":
+            deleteProduct()
             break
         case "5":
             alert('Saindo do programa de gerenciamento de estoque')
             break
         default:
-            alert('Opção inválida,tente novamente')
+            alert('Opção inválida')
     }
 }
