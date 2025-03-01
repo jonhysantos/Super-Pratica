48fa4cbfdfedac1a0190c66179571719bef4fd81
Jonhy Santos <jonhy1905@hotmail.com>
2025-03-01 09:09:07 -0300

    pratica do dia.

--- Project07/Games-list04/script.js
+++ Project07/Games-list04/script.js
@@ -3,33 +3,31 @@ function toKebabCase(str){
 }
 
 function addLi(game){
-    const liElement = document.createElement('li');
-    liElement.id = toKebabCase(game.name);
-    liElement.innerHTML = `<p>Nome: ${game.name}</p>`
-    liElement.innerHTML += `<p>Ano de lançamento: ${game.year}</p>`
-    liElement.innerHTML += `<p>Gênero: ${game.genero}`
-    document.getElementById('list-games').appendChild(liElement)
+    const li = document.createElement('li');
+    li.id = toKebabCase(game.name)
+    li.innerHTML = `Nome: ${game.name}. <br> Ano de lançamento: ${game.year}. <br> Gênero: ${game.genero}`
+    document.getElementById('list-games').appendChild(li)
 }
 
-const form = document.getElementById('register-games');
+const form = document.getElementById('register-games')
 form.addEventListener('submit',function(event){
     event.preventDefault()
 
-    const inputName = document.getElementById('input-name')
-    const inputYear = document.getElementById('input-year')
-    const inputGenero = document.getElementById('input-genero')
-    if(inputName.value.length == 0){
-        alert('Preencha todos os campos')
+    const inputName = document.getElementById('input-name');
+    const inputYear = document.getElementById('input-year');
+    const inputGenero = document.getElementById('input-genero');
+
+    if(inputName.value.langth == 0 || inputYear.value.length == 0 || inputGenero.value.length == 0){
+        alert('Preencha os campos necessários')
         return
     }
 
     const games = []
+
     games.name = inputName.value
     games.year = Number(inputYear.value)
     games.genero = inputGenero.value
 
     addLi(games)
-
-    
     event.target.reset()
 })
