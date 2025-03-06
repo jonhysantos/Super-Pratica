8053da58149b603558bee709f4abfb3818b463dc
Jonhy Santos <jonhy1905@hotmail.com>
2025-03-06 15:57:57 -0300

    pratica do dia

--- Project08/Games10/scripts.js
+++ Project08/Games10/scripts.js
@@ -1,14 +1,13 @@
-import { games } from "./products.js";
+import {games} from './products.js'
 
-const btnShowAll = document.querySelector('.show-all')
+const btnShowAll = document.querySelector('.show-all');
 const list = document.querySelector('ul');
 const btnMapAll = document.querySelector('.map-all');
 const btnSumAll = document.querySelector('.sum-all');
-const btnFilerAll = document.querySelector('.filter-all');
+const btnFilterAll = document.querySelector('.filter-all');
 
 function currencyReal(value){
-    const real = value.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
-    return real
+    return value.toLocaleString('pt-BR',{style:'currency',currency:'BRl'})
 }
 
 function showAll(arrayProduct){
@@ -20,33 +19,31 @@ function showAll(arrayProduct){
                 <p>${product.name}</p>
                 <p class="item-price">${currencyReal(product.price)}</p>
             </li>
-        
         `
     })
-
     list.innerHTML = myLi
 }
 
 function mapAll(){
     const newPrices = games.map(product => ({
-        name:product.name,
-        price:product.price * 0.43,
-        src:product.src
+        ...product,
+        price:product.price * 0.43
     }))
     showAll(newPrices)
 }
 
 function sumAll(){
     const total = games.reduce((acc,curr) => acc + curr.price,0)
-    list.innerHTML = `O total ${currencyReal(total)}`
+    list.innerHTML = `O total é de ${currencyReal(total)}`
+    list.style.fontSize = '2em'
 }
 
 function filterAll(){
-    const justHorror = games.filter(product => product.genero === "Survivor horror")
+    const justHorror = games.filter(product => product.genero === 'Survivor horror')
     showAll(justHorror)
 }
 
 btnShowAll.addEventListener('click',() => showAll(games))
 btnMapAll.addEventListener('click',mapAll)
 btnSumAll.addEventListener('click',sumAll)
-btnFilerAll.addEventListener('click',filterAll)
+btnFilterAll.addEventListener('click',filterAll)
