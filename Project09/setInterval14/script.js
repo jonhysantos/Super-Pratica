03c886f04b052f95ebd449a273f2b77041f5945f
Jonhy Santos <jonhy1905@hotmail.com>
2025-04-18 21:38:46 -0300

    pratica do dia

--- Project09/setInterval14/script.js
+++
@@ -1,9 +0,0 @@
-function code(){
-    const random = Math.floor(Math.random() * 999999) + 1
-    const padded = random.toString().padStart(6,'0')
-    const code = document.getElementById('confirmation-code')
-
-    code.innerHTML = padded  
-}
-
-setInterval(code, 10 * 1000)
