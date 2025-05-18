0cadb3334c10e2ebab1f5ee0578fe1c0391714c7
Jonhy Santos <jonhy1905@hotmail.com>
2025-05-17 22:38:18 -0300

    pratica

--- Project06/Click04/index.js
+++
@@ -1,9 +0,0 @@
-const p = document.querySelector('.p');
-const btn = document.querySelector('.btn');
-
-let counter = 0;
-
-btn.addEventListener('click',function(){
-    counter ++
-    p.textContent = `${counter} clique${counter !== 1 ? 's':''}`
-})
