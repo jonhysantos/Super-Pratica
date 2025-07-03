52fd577f7daf7a7f611b05236adadfde994b3ec3
Jonhy Santos <jonhy1905@hotmail.com>
2025-07-03 09:29:57 -0300

    pratica

---
+++ Project09/setInterval18/script.js
@@ -0,0 +1,8 @@
+function generateCode(){
+    const random = Math.floor(Math.random() * 999999) + 1
+    const padded = random.toString().padStart(6,'0')
+    const confirmation = document.getElementById('confirmation-code')
+    confirmation.textContent = padded
+}
+
+setInterval(generateCode, 10 * 1000)
