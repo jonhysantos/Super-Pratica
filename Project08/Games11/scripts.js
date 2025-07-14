b7b9f6f238988abec94a25af54d2786d946b2b49
Jonhy Santos <jonhy1905@hotmail.com>
2025-07-14 12:28:17 -0300

    pratica

--- Project08/Games11/scripts.js
+++ Project08/Games11/scripts.js
@@ -4,8 +4,7 @@ const ulElement = document.querySelector('ul');
 const btnShowAll = document.querySelector('.show-all');
 const btnMapAll = document.querySelector('.map-all');
 const btnSumAll = document.querySelector('.sum-all');
-const btnFilterAll = document.querySelector('.filter-all')
-
+const btnFilerAll = document.querySelector('.filter-all');
 
 function showAll(arrayProduct){
     let myLi = ''
@@ -14,10 +13,11 @@ function showAll(arrayProduct){
             <li>
                 <img src=${product.src}>
                 <p>${product.name}</p>
-                <p class='item-price'>${product.price}</p>
+                <p class="item-price">${product.price}</p>
             </li>
         `
     })
+
     ulElement.innerHTML = myLi
 }
 
@@ -30,17 +30,17 @@ function mapAll(){
 }
 
 function sumAll(){
-    const total = games.reduce((acc,curr) => acc + curr.price,0)
-    ulElement.innerHTML = `O valor total é de ${total}`
+    const total = games.reduce((acc,curr) => acc + curr.price,0 )
+    ulElement.innerHTML = `O total é de ${total}`
     ulElement.style.fontSize = '2em'
 }
 
 function filterAll(){
-    const justHorror = games.filter(product => product.genero === "Survivor horror")
-    console.log(justHorror)
+    const justHorror = games.filter(product => product.genero === 'Survivor horror')
     showAll(justHorror)
 }
+
 btnShowAll.addEventListener('click',() => showAll(games))
-btnMapAll.addEventListener('click',mapAll);
+btnMapAll.addEventListener('click',mapAll)
 btnSumAll.addEventListener('click',sumAll)
-btnFilterAll.addEventListener('click',filterAll)
+btnFilerAll.addEventListener('click',filterAll)
