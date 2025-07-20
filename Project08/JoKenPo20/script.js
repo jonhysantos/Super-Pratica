972a2390d25fd523712a572af7a4cb7c0f8f4f12
Jonhy Santos <jonhy1905@hotmail.com>
2025-07-20 13:45:28 -0300

    update

--- Project08/JoKenPo20/script.js
+++ Project08/JoKenPo20/script.js
@@ -1,5 +1,32 @@
 
-
-const playHuman = (humanChoice) => {
-    console.log(humanChoice)
+const result = document.querySelector('.result')
+const GAME_OPTIONS = {
+    ROCK:'rock',
+    PAPER:'paper',
+    SCISSORS:'scissors'
 }
+const playHuman = (humanChoice) => {
+    playGame(humanChoice,playMachine())
+}
+
+const playMachine = () => {
+    const options = [GAME_OPTIONS.ROCK,GAME_OPTIONS.SCISSORS,GAME_OPTIONS.PAPER]
+    const random = Math.floor(Math.random() * 3)
+    return options[random]
+}
+
+const playGame = (human,machine) => {
+    console.log(human,machine)
+    if(human === machine){
+        result.innerHTML = 'Deu empate!'
+    }else if(human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS
+        || (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK
+        || (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
+        )
+    ){
+        result.innerHTML = 'Parabéns! Você venceu'
+    }else{
+        result.innerHTML = "Perdeu para a Alexa"
+    }
+}
+
